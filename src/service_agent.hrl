055f8dae63ca54ecdfc13be55cb8bcd4318ce80e
Aaron Spiegel <spiegela@gmail.com>
2015-01-05 21:42:57 -0600

    Improve types

--- src/service_agent.hrl
+++ src/service_agent.hrl
@@ -1,1 +1,13 @@
--record(instance, {name, cookie, dist_min, dist_max}).
+-type string_list() :: [nonempty_string()].
+-type service_attr_input() :: { address | cookie | dist_min | dist_max,
+								string()
+							  }.
+-type service_attr_list() :: [service_attr_input()].
+
+-record(instance, {
+    name      :: undefined | atom(),
+    bind_addr :: undefined | string(),
+    cookie    :: undefined | atom(),
+    dist_min  :: undefined | integer(),
+    dist_max  :: undefined | integer()
+  }).
