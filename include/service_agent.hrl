969eae792ad9144f408cab70c0749f7a460849fb
Aaron Spiegel <spiegela@gmail.com>
2015-01-11 09:13:23 -0600

    Allow agent_instance type spec to work with ets:match

--- include/service_agent.hrl
+++ include/service_agent.hrl
@@ -5,9 +5,9 @@
 -type service_attr_list() :: [service_attr_input()].
 
 -record(agent_instance, {
-    instance_id :: undefined | atom(),
-    bind_addr   :: undefined | string(),
-    cookie      :: undefined | atom(),
-    dist_min    :: undefined | pos_integer(),
-    dist_max    :: undefined | pos_integer()
+    instance_id :: undefined | atom() | '_',
+    bind_addr   :: undefined | string() | '_',
+    cookie      :: undefined | atom() | '_',
+    dist_min    :: undefined | pos_integer() | '_',
+    dist_max    :: undefined | pos_integer() | '_'
   }).
