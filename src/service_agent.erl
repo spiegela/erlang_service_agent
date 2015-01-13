10346d432b85e2e9e051128a2cc44fb252741de9
Aaron Spiegel <spiegela@gmail.com>
2015-01-13 09:07:23 -0600

    Enable registry with broker

--- src/service_agent.erl
+++ src/service_agent.erl
@@ -13,7 +13,7 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, { leader :: string() | false }).
 
 %%% API
 
@@ -32,20 +32,28 @@ list() ->
 %%% gen_server callbacks
 
 init([]) ->
-    {ok, #state{}}.
+  Leader = os:getenv("LEADER"),
+  register_agent(Leader),
+  {ok, #state{agent = Leader}.
 
 handle_call({create, Instance}, _From, State) ->
-    {reply, service_instance:start(Instance), State};
+  {reply, service_instance:start(Instance), State};
 handle_call({delete, Instance}, _From, State) ->
-    {reply, service_instance:stop(Instance), State};
+  {reply, service_instance:stop(Instance), State};
 handle_call(list, _From, State) ->
-    {reply, service_instance:list(), State}.
+  {reply, service_instance:list(), State}.
 
 handle_cast(stop, State) ->
   {stop, normal, State};
 handle_cast(_Request, State) ->
   {noreply, State}.
 
+handle_info(register, #state{agent = Leader}=State) ->
+  register_agent(Leader),
+  {noreply, State};
+handle_info(deregister, #state{agent = Leader}=State) ->
+  deregister_agent(Leader),
+  {noreply, State};
 handle_info(_Info, State) ->
   {noreply, State}.
 
@@ -56,3 +64,23 @@ code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
 %%% Internal functions
+
+-spec deregister_agent(string()) -> ok.
+deregister_agent(LeaderIP) -> registry_action(deregister, LeaderIP).
+
+-spec register_agent(string()) -> ok.
+register_agent(LeaderIP) -> registry_action(register, LeaderIP).
+
+-spec registry_action(atom(), string()) -> ok.
+registry_action(_Action, false) ->
+  ok;
+registry_action(Action, LeaderIP) ->
+  try
+    rpc:call(broker(LeaderIP), service_agent_registry, Action, [node()], 5000)
+  catch {badrpc, timeout} ->
+    erlang:send_after(?REG_INTERVAL, self(), Action)
+  end.
+
+-spec broker(string()) -> atom().
+broker(IP) ->
+  Broker = list_to_atom(lists:concat(["service_broker@", IP])),
